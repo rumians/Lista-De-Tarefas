92b9d7be863fccfb569ae2db25aaf6f2e1ccbd09
rumians <ruanfort12@gmail.com>
2023-11-30 09:30:18 -0300

    taindo

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function entrada() {
-    
+let add = document.getElementById("descricao");
+let pendentes = document.getElementById("tarefas");
+let finalizadas = document.getElementById("finalizadas");
+let blud = ""
+
+
+add.addEventListener("change", function () {
+    blud = add.value;
+})
+
+function adicionar() {
+    pendentes.innerHTML += `<li> ${blud} <button onclick="finalizar()" >Finalizar</button> </li> `;
+}
+
+function finalizar() {
+    finalizadas.innerHTML += `<li> ${blud} </li>`;
+    pendentes.
 }
