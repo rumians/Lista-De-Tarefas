35c951dacb2afbaac5cfdc27426964402f69f0fb
rumians <ruanfort12@gmail.com>
2023-12-07 22:02:20 -0300

    tentei

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 let add = document.getElementById("descricao");
 let pendentes = document.getElementById("tarefas");
 let finalizadas = document.getElementById("finalizadas");
+let btnApagar = '';
 let valoradd = "";
 
 function adicionar() {
     valoradd = add.value;
     const novoItem = document.createElement('li');
-    //novoItem.innerHTML += `${valoradd} <button onclick="finalizar(this.parentElement)" >Finalizar</button>  <button onclick="Apagar(novoItem)">Apagar<button>`;
     const textoItem = valoradd;
     novoItem.innerHTML += textoItem;
+    btnApagar = document.createElement("button");
+    btnApagar.textContent="Apagar";
     let btnFim = document.createElement("button");
-    btnFim.textContent="Finalizar"
+    btnFim.textContent="Finalizar";
+    
     btnFim.addEventListener("click", function() {
         finalizar(textoItem);
+        Apagar(textoItem);
     })
+
     novoItem.appendChild(btnFim);
-    //<button onclick="finalizar(this.parentElement)" >Finalizar</button>  <button onclick="Apagar(novoItem)" >Apagar</
     pendentes.appendChild(novoItem);
     add.focus();
     add.value = "";
@@ -23,11 +27,7 @@ function adicionar() {
 
 function finalizar(itemdalista) {
     finalizadas.innerHTML += `<li> ${itemdalista} </li>`;
-    // itemdalista.style.display = "none";
-}
-
-function Apagar(itemdalista) {
-    itemdalista.style.display = "none";
+    
 }
 
 add.addEventListener("keypress", (e) => {
